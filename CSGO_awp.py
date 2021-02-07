4846eb78c9f52b8d821172441ae33d5816d207d5
arincdemir <demir.arinc02@gmail.com>
2021-02-07 18:16:30 +0300

    added hotkey listening

--- CSGO_awp.py
+++ CSGO_awp.py
@@ -9,7 +9,7 @@ import PySimpleGUI as ps
 GUITitle = "CSGO Clicker"
 ps.theme("DarkGreen3")
 layout = [
-    [ps.Text(text="Waiting for hotkey",key="-Text-")],
+    [ps.Text(text="Set a hotkey by pressing the button.",key="-Text-", size=(50,4))],
     [ps.Button("Set Hotkey")]
 ]
 window = ps.Window(GUITitle, layout, finalize=True)
@@ -20,15 +20,37 @@ firePoint = (pyautogui.size()[0] / 2 + 1, pyautogui.size()[1] / 2 + 1)
 keyDown = False
 running = True
 
+#gets the initial hotkey
+event, values = window.read()
+if(event == ps.WINDOW_CLOSED):
+        running = False
+elif(event == "Set Hotkey"):
+    window["-Text-"].update("Press key")
+    window.Finalize()
+    key = keyboard.read_key()
+print(key)
+
+#the program
+window["-Text-"].update("Press hotkey after placing your crosshair")
 while running:
     event, values = window.read(timeout=0.1)
     if(event == ps.WINDOW_CLOSED):
         running = False
+    elif(event == "Set Hotkey"):
+        window["-Text-"].update("Press key")
+        window.Finalize()
+        key = keyboard.read_key()
+    
     print("not listening")
+
     if(keyboard.is_pressed(key) and keyDown == False):
+        window["-Text-"].update("Watching the crosshair")
+        event, values = window.read(timeout=0.1)
+        if(event == ps.WINDOW_CLOSED):
+            running = False
         keyDown = True
         initialPixel = pyautogui.screenshot().getpixel(firePoint)
-        while pyautogui.screenshot().getpixel(firePoint) == initialPixel:
+        while pyautogui.screenshot().getpixel(firePoint) == initialPixel and running:
             print("listening")
             time.sleep(0.01)
             if(keyboard.is_pressed(key) and keyDown == False):
@@ -38,6 +60,8 @@ while running:
                 keyDown = False
         else:
             pyautogui.click()
+        window["-Text-"].update("Press hotkey after placing your crosshair")
+
     elif(not keyboard.is_pressed(key)):
         keyDown = False
 
