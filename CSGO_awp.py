a8b0c4d91b325cf737d525ac0970b2c1d06af606
arincdemir <demir.arinc02@gmail.com>
2021-02-07 16:31:45 +0300

    removed unnecessary code

--- CSGO_awp.py
+++ CSGO_awp.py
@@ -3,14 +3,11 @@ import pyautogui
 import keyboard
 import PySimpleGUI as ps
 
-## consider using python 3.7 for some libraries
+# consider using python 3.7 for some libraries
 
 key = "up"
 firePoint = (pyautogui.size()[0] / 2 + 1, pyautogui.size()[1] / 2 + 1)
 keyDown = False
-
-#comment comment feature comment
-#main comment comment main
 
 while True:
     print("not listening")
@@ -31,6 +28,3 @@ while True:
         keyDown = False
 
     time.sleep(0.01)
-
-    def mainFunc():
-        pass
