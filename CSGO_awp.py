9b212156a86d462dec3181a21e091e9623d509d7
arincdemir <demir.arinc02@gmail.com>
2021-02-07 17:05:58 +0300

    added initial gui setup

--- CSGO_awp.py
+++ CSGO_awp.py
@@ -5,11 +5,25 @@ import PySimpleGUI as ps
 
 # consider using python 3.7 for some libraries
 
+#pysimplegui setup
+GUITitle = "CSGO Clicker"
+ps.theme("DarkGreen3")
+layout = [
+    [ps.Text(text="Waiting for hotkey",key="-Text-")],
+    [ps.Button("Set Hotkey")]
+]
+window = ps.Window(GUITitle, layout, finalize=True)
+
+#program setup
 key = "up"
 firePoint = (pyautogui.size()[0] / 2 + 1, pyautogui.size()[1] / 2 + 1)
 keyDown = False
+running = True
 
-while True:
+while running:
+    event, values = window.read(timeout=0.1)
+    if(event == ps.WINDOW_CLOSED):
+        running = False
     print("not listening")
     if(keyboard.is_pressed(key) and keyDown == False):
         keyDown = True
@@ -28,3 +42,5 @@ while True:
         keyDown = False
 
     time.sleep(0.01)
+
+window.close()
